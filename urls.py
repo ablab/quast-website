40f862daaa8a7321b54e3abd9c2d7632cc9c17fe
vladsaveliev <vladsaveliev@me.com>
2014-10-01 13:38:53 +0400

    Allowed hosts for settings

--- urls.py
+++ urls.py
@@ -15,10 +15,10 @@ logger = logging.getLogger('quast')
 
 
 urlpatterns = patterns('',
-    url(r'^robots\.txt$', RedirectView(url='/static/robots.txt')),
-    url(r'^sitemap\.xml$', RedirectView(url='/static/sitemap.xml')),
-    url(r'^excanvas\.min\.js$', RedirectView(url='/static/float/excanvas.min.js')),
-    url(r'^favicon\.ico$', RedirectView(url=('/static/img/favicon_debug.ico'
+    url(r'^robots\.txt$', RedirectView.as_view(url='/static/robots.txt')),
+    url(r'^sitemap\.xml$', RedirectView.as_view(url='/static/sitemap.xml')),
+    url(r'^excanvas\.min\.js$', RedirectView.as_view(url='/static/float/excanvas.min.js')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=('/static/img/favicon_debug.ico'
                                              if settings.DEBUG
                                              else '/static/img/favicon.ico')
                                        )
